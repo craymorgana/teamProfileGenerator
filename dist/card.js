3f629b9bde1c216242a6289cd586c91092b23bad
Aaron Morgan <aaron.morgan.7@outlook.com>
2022-11-20 15:35:44 -0700

    html file is now generated upon completion of the team

---
+++ dist/card.js
@@ -0,0 +1,7 @@
+import { employees } from "../index.js";
+for (let i = 0; i < employees.length; i++) {
+	const employeeCard = `<h2>${employees[i].name}</h2>
+					<h3 class="role">${employees[i].getRole}</h3>`;
+
+	$(".card").append(employeeCard);
+}
--- index.js
+++ index.js
@@ -9,7 +9,139 @@ const Manager = require("./lib/Manager");
 
 let employees = [];
 
-const addManager = () => {
+const generateHTML = ({}) =>
+	`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="style.css">
+      <title>My Team</title>
+    </head>
+    <body>
+
+    <header>
+        <h1 class="">My Team</h1>
+    </header>
+
+    <main>
+    <div class="card">
+
+    </div>
+
+    </main>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="../index.js"></script>
+	<script src="../src/card.js"></script>
+    </body>
+    </html>`;
+
+const addMoreEmployees = () => {
+	inquirer
+		.prompt([
+			{
+				type: "list",
+				message: "Do you wish to add an engineer, intern, or finish?",
+				name: "option",
+				choices: ["engineer", "intern", "finish"],
+			},
+		])
+		.then((answers) => {
+			if (answers.option === "engineer") {
+				inquirer
+					.prompt([
+						{
+							type: "input",
+							name: "name",
+							message: "Enter the team engineer's name:",
+						},
+						{
+							type: "input",
+							name: "id",
+							message: "Enter the engineer's ID: ",
+						},
+						{
+							type: "input",
+							name: "email",
+							message: "Enter the engineer's email: ",
+						},
+						{
+							type: "input",
+							name: "github",
+							message: "Enter the engineer's Github username: ",
+						},
+					])
+					.then((answers) => {
+						const engineer = new Engineer(
+							answers.name,
+							answers.id,
+							answers.email,
+							answers.github
+						);
+
+						employees.push(engineer);
+						addMoreEmployees();
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			}
+
+			if (answers.option === "intern") {
+				inquirer
+					.prompt([
+						{
+							type: "input",
+							name: "name",
+							message: "Enter the team intern's name:",
+						},
+						{
+							type: "input",
+							name: "id",
+							message: "Enter the intern's ID: ",
+						},
+						{
+							type: "input",
+							name: "email",
+							message: "Enter the intern's email: ",
+						},
+						{
+							type: "input",
+							name: "school",
+							message: "Enter the intern's school: ",
+						},
+					])
+					.then((answers) => {
+						const intern = new Intern(
+							answers.name,
+							answers.id,
+							answers.email,
+							answers.school
+						);
+						employees.push(intern);
+						addMoreEmployees();
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			}
+
+			const createHTML = () => {
+				const htmlContent = generateHTML(employees);
+
+				fs.writeFile("./dist/index.html", htmlContent, (err) =>
+					err
+						? console.log(err)
+						: console.log("Successfully created index.html!")
+				);
+			};
+			createHTML();
+		})
+		.catch((err) => {});
+};
+
+const addFirstEmployee = () => {
 	inquirer
 		.prompt([
 			{
@@ -42,131 +174,16 @@ const addManager = () => {
 			);
 
 			employees.push(manager);
+
+			addMoreEmployees();
 		})
-		.catch((err) => {});
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
-const addNewEmployee = () => {
-	inquirer
-		.prompt([
-			{
-				type: "list",
-				message: "Do you wish to add an engineer, intern, or finish?",
-				name: "option",
-				choices: ["engineer", "intern", "finish"],
-			},
-		])
-		.then((answers) => {
-			if (answers === "engineer") {
-				inquirer
-					.prompt([
-						{
-							type: "input",
-							name: "name",
-							message: "Enter the team engineer's name:",
-						},
-						{
-							type: "input",
-							name: "id",
-							message: "Enter the engineer's ID: ",
-						},
-						{
-							type: "input",
-							name: "email",
-							message: "Enter the engineer's email: ",
-						},
-						{
-							type: "input",
-							name: "github",
-							message: "Enter the engineer's Github username: ",
-						}.then((answers) => {
-							const engineer = new Engineer(
-								answers.name,
-								answers.id,
-								answers.email,
-								answers.github
-							);
+addFirstEmployee();
 
-							employees.push(engineer);
-
-							addNewEmployee();
-						}),
-					])
-					.catch((err) => {});
-			}
-			if (answers === "intern") {
-				inquirer
-					.prompt([
-						{
-							type: "input",
-							name: "name",
-							message: "Enter the team intern's name:",
-						},
-						{
-							type: "input",
-							name: "id",
-							message: "Enter the intern's ID: ",
-						},
-						{
-							type: "input",
-							name: "email",
-							message: "Enter the intern's email: ",
-						},
-						{
-							type: "input",
-							name: "school",
-							message: "Enter the intern's school: ",
-						}.then((answers) => {
-							const intern = new Intern(
-								answers.name,
-								answers.id,
-								answers.email,
-								answers.school
-							);
-
-							employees.push(intern);
-
-							addNewEmployee();
-						}),
-					])
-					.catch((err) => {});
-			}
-		})
-		.catch((err) => {});
-};
-
-addManager();
-addNewEmployee();
-
-const generateHTML = (employees) => {
-	`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="./src/style.css">
-      <title>My Team</title>
-    </head>
-    <body>
-      
-    <header>
-        <h1 class="">My Team</h1>
-    </header>
-       
-    <main>
-    <div class="card">
-        
-    
-    </div>
-    
-    </main>
-    </body>
-    </html>`;
-};
-
-fs.writeFile("./dist/index.html", generateHTML, (err) =>
-	err ? console.log(err) : console.log("Successfully created HTML")
-);
 // WHEN I decide to finish building my team
 // THEN I exit the application, and the HTML is generated
 
